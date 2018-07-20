7e61bdc8b24fa5838488c027dc429965e731a070
Lauren <law9779@gmail.com>
2018-07-19 17:08:12 -0700

    deleted the table view cell and added the collection view cell

--- BuzzDeals/Controllers/HomeViewController.swift
+++ BuzzDeals/Controllers/HomeViewController.swift
@@ -8,48 +8,34 @@
 
 import UIKit
 
-class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class HomeViewController: UIViewController, UITableViewDelegate {
     
     //Mark: - Properties
     
     @IBOutlet weak var tableView: UITableView!
     
-    @IBAction func unwindWithSegue(_ segue: UIStoryboardSegue) {
-        
-    }
-    
-    var postData = [String]()
-    
-    //Mark - VC Lifecycle
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //do any additional setup
-        tableView.delegate = self
-        tableView.dataSource = self
-
+     
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         //dispose of any resources that can be recreated
     }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return postData.count
+        // 1
+        return 10
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: indexPath)
-    UITableViewCell {
-    
-    return postData
-    
+    private func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 2
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listNotesTableViewCell", for: indexPath)
+        cell.textLabel?.text = "Cell Row: \(indexPath.row) Section: \(indexPath.section)"
+        
+        return cell
     }
 }
-// MARK: - UITableViewDelegate
-
-
-
-
 
