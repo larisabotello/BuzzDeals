a03a1c5fe9dc03b9cec364ddec4d1e3947851df8
Larisa Botello <larisa.botello01@utrgv.edu>
2018-07-20 12:01:17 -0700

    Code to retrieve data transferred to homeviewcontroller

--- BuzzDeals/Controllers/HomeViewController.swift
+++ BuzzDeals/Controllers/HomeViewController.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseUI
+import FirebaseDatabase
+
+class HomeViewController: UIViewController{
+    var ref: DatabaseReference!
+    var dealsList = [Deals]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //THIS IS WHERE I READ DATA -LARISA
+        ref = Database.database().reference().child("Deals")
+        //addDeals()
+        ref.observe(DataEventType.value, with: {(snapshot) in
+            if snapshot.childrenCount > 0{
+                self.dealsList.removeAll()
+                for deal in snapshot.children.allObjects as![DataSnapshot]{
+                    let dealsObject = deal.value as?[String:AnyObject]
+                    let dealName = dealsObject?["Name"]
+                    let dealLocation = dealsObject?["Location"]
+                    let deals = dealsObject?["Deal"]
+                    let data = Deals(name: (dealName as! String), location: dealLocation as! String, deal: deals as! String)
+                    self.dealsList.append(data)
+                }
+                //tersting data retrieval
+                print(self.dealsList[0].deal)
+            }
+        })
+        
+    }
+}
--- BuzzDeals/Controllers/LoginViewController.swift
+++ BuzzDeals/Controllers/LoginViewController.swift
@@ -15,29 +15,11 @@ import FirebaseDatabase
 typealias FIRUser = FirebaseAuth.User
 
 class LoginViewController: UIViewController {
-    var ref: DatabaseReference!
-    var dealsList = [Deals]()
     @IBOutlet weak var loginButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //THIS IS WHERE I READ DATA -LARISA
-        ref = Database.database().reference().child("Deals")
-        //addDeals()
-        ref.observe(DataEventType.value, with: {(snapshot) in
-            if snapshot.childrenCount > 0{
-                self.dealsList.removeAll()
-                for deal in snapshot.children.allObjects as![DataSnapshot]{
-                    let dealsObject = deal.value as?[String:AnyObject]
-                    let dealName = dealsObject?["Name"]
-                    let dealLocation = dealsObject?["Location"]
-                    let deals = dealsObject?["Deal"]
-                    let data = Deals(name: (dealName as! String), location: dealLocation as! String, deal: deals as! String)
-                    self.dealsList.append(data)
-                }
-            }
-        })
     }
 //    func addDeals(){
 //        let key = ref.childByAutoId().key
@@ -61,8 +43,6 @@ class LoginViewController: UIViewController {
         
         let authViewController = authUI.authViewController()
         present(authViewController, animated: true)
-        //tersting data retrieval
-        print(dealsList[0].deal)
     }
     
 }
