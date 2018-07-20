31339f00b334df78565dbeefa9bc7dc248cc94f9
Larisa Botello <larisa.botello01@utrgv.edu>
2018-07-20 14:44:38 -0700

    Got nothing done but alot of new code was added?

--- BuzzDeals/Controllers/HomeViewController.swift
+++ BuzzDeals/Controllers/HomeViewController.swift
@@ -13,13 +13,32 @@ import FirebaseAuth
 import FirebaseUI
 import FirebaseDatabase
 
-class HomeViewController: UIViewController{
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    @IBOutlet weak var tableView: UITableView!
+    
     var ref: DatabaseReference!
     var dealsList = [Deals]()
+    
+    //    //TableView Functions Needed
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return (dealsList.count)
+    }
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ViewControllerTableViewCell
 
+            cell.dataName.text = "testing"
+            //print (cell.dataName.text)
+               // cell.dataDeal.text = "testing"
+                //cell.dataLocation.text = "testAgain"
+//                cell.dataName.text = dealsList[indexPath.row].name
+//                cell.dataDeal.text = dealsList[indexPath.row].deal
+//                cell.dataLocation.text = dealsList[indexPath.row].location
+
+        return (cell)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         //THIS IS WHERE I READ DATA -LARISA
         ref = Database.database().reference().child("Deals")
         //addDeals()
@@ -34,10 +53,30 @@ class HomeViewController: UIViewController{
                     let data = Deals(name: (dealName as! String), location: dealLocation as! String, deal: deals as! String)
                     self.dealsList.append(data)
                 }
-                //tersting data retrieval
+                //testing data retrieval
                 print(self.dealsList[0].deal)
             }
         })
         
     }
+    
 }
+//extension HomeViewController: UITableViewDataSource, UITableViewDelegate{
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return (dealsList.count)
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell =  tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ViewControllerTableViewCell
+//        //            cell.dataName.text = "testing"
+//        //            print (cell.dataName.text)
+//        //               // cell.dataDeal.text = "testing"
+//        //                //cell.dataLocation.text = "testAgain"
+//                        cell.dataName.text = dealsList[indexPath.row].name
+//        ////                cell.dataDeal.text = dealsList[indexPath.row].deal
+//        ////                cell.dataLocation.text = dealsList[indexPath.row].location
+//        //
+//                return (cell)
+//           }
+//
+//}
