63a72f3ba0f45fff6a7b79d553597d2d3d4f3d99
Larisa Botello <larisa.botello01@utrgv.edu>
2018-07-20 13:24:06 -0700

    TableView: Progress1
    Added file for Protoype cell

---
+++ BuzzDeals/Controllers/ViewControllerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ViewControllerTableViewCell.swift
+//  BuzzDeals
+//
+//  Created by Larisa Botello on 7/20/18.
+//  Copyright © 2018 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
